bebf90cdc424eb556d7a7c861b686b186de0c5db
ryancartoon <ryancartoon@appletekiMacBook-Air-3.local>
2019-09-19 20:46:37 +0800

    add test for test api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,8 @@
 import json
 
-from unittest.mock import patch
+# from unittest.mock import patch
 
-from app import models
+# from app import models
 from app.api import add_task
 
 
@@ -10,6 +10,16 @@ def test_tasks(test_app):
     expect_tasks = [add_task(), add_task()]
 
     client = test_app.test_client()
-    resp = client.get("/tasks")
+    resp = client.get("/api/tasks")
 
-    assert [t["uuid"] for t in json.loads(resp.data)] == [t.uuid for t in expect_tasks]
+    assert [t["uuid"]
+            for t in json.loads(resp.data)] == [t.uuid for t in expect_tasks]
+
+
+def test_task(test_app):
+    task = add_task()
+
+    client = test_app.test_client()
+    resp = client.get("/api/tasks/" + task.uuid)
+
+    assert task.uuid == resp.json["uuid"]
